c3651848649dc252e41f2e177032d97867eb1536
Davino Heyse <davino.heyse@campuskompas.be>
2023-11-20 10:31:03 +0100

    1ste oefeningen volledig

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
-    public static void main(String[] args){
-        System.out.println("test");
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setAge(-12);
+        person.setFirstName("Davino");
+        person.setLastName("Heyse");
+        System.out.println("Leeftijd: " + person.getAge());
+        System.out.println("Naam :  " + person.getFirstName() + " " + person.getLastName());
     }
 }
+
+
